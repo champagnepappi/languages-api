fc66378eee33c52b58fddd5bef54dd84a9b613a9
kevin <kevinochieng548@gmail.com>
2018-03-04 17:06:35 +0300

    return one element from the dictionary

--- restful.py
+++ restful.py
@@ -12,6 +12,11 @@ def test():
 def returnAll():
     return jsonify({'languages': languages})
 
+@app.route('/languages/<string:name>', methods=['GET'])
+def returnOne(name):
+    langs = [language for language in languages if language['name'] == name]
+    return jsonify({'language': langs[0]})
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
