a82cf50f655d8c1c253c2f82e5679229b2d1d5f9
kevin <kevinochieng548@gmail.com>
2018-03-04 18:00:23 +0300

    update elements in dictionary in the editOne method

--- restful.py
+++ restful.py
@@ -17,6 +17,19 @@ def returnOne(name):
     langs = [language for language in languages if language['name'] == name]
     return jsonify({'language': langs[0]})
 
+@app.route('/languages', methods=['POST'])
+def addOne():
+    language = {'name': request.json['name']}
+    languages.append(language)
+    return jsonify({'languages': languages})
+
+@app.route('/languages/<string:name>', methods=['PUT'])
+def editOne(name):
+    langs = [language for language in languages if language['name'] == name]
+    langs[0]['name'] = request.json['name']
+    return jsonify({'language': langs[0]})
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
