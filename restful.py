17f5e99f309ca7fc70db32001615e619b56dff43
kevin <kevinochieng548@gmail.com>
2018-03-04 16:47:54 +0300

    define the test method

--- restful.py
+++ restful.py
@@ -3,3 +3,11 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 languages = [{'name': 'Javascript'}, {'name': 'Ruby'}, {'name': 'Python'}]
+
+@app.route('/', methods=['GET'])
+def test():
+    return jsonify({'message': 'It works!'})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
